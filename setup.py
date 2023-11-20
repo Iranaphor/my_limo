2d649eb27d2d7ff667d69937aae5d0cb791f5b12
James R Heselden <primordia@live.com>
2023-11-20 10:44:33 +0000

    initial package

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages
+from setuptools import setup
+from glob import glob
+
+package_name = 'my_limo'
+
+setup(
+    name=package_name,
+    version='3.0.0',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml'])
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='James Heselden',
+    maintainer_email='jheselden@lincoln.ac.uk',
+    description='The my_limo package',
+    license='MIT',
+    tests_require=['pytest', 'launch-pytest'],
+    entry_points={
+        'console_scripts': [
+#            'bob.py = my_limo.scripts.bob:main',
+        ],
+    },
+
+)
