fa3d4f7023f6579cc9c2cfad44b9babaa29d54b8
James R Heselden <primordia@live.com>
2024-02-19 16:31:57 +0000

    Extra configs

--- bash/local_bashrc.sh
+++ bash/local_bashrc.sh
@@ -7,3 +7,12 @@ function mu () { tmule -c /workspaces/limo_ros2/src/custom_packages/my_limo/tmul
 function vr () { tmule -c /workspaces/limo_ros2/src/custom_packages/vrviz_ros/tmule/example.tmule.yaml $1 ; }
 
 function topogoal () { ros2 action send_goal /topological_navigation topological_navigation_msgs/action/GotoNode "{target: $1}" ; }
+
+export OPENAI_PROMPT="You are a small robot which drives around office rooms to deliver mail, your name is LIMO."
+export ELEVENLABS_VOICE="Paul"
+
+export DOCKER_ESPEAK=true
+export ALSA_OUTPUT='hw:2,0'
+export ALSA_INPUT='hw:3,0'
+
+source /workspaces/limo_ros2/src/custom_packages/my_limo/bash/secrets.sh
